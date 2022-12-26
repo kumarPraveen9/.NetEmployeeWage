bc5aad09dc614ee35e16dd4204c1ea040ce63e8d
kumarPraveen9 <gpk50626@gmail.com>
2022-12-26 14:18:00 +0530

    modifed for UC2

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@ namespace Emloyee
 {
     class Program
     {
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program \n");
-            EmployeeAttendance.checkatt();
+            // EmployeeAttendance.checkatt();
+            Console.WriteLine("====================================================================");
+            RegularWage.Cal_Rwage();
         }
     }
 }
