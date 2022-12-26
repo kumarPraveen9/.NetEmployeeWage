bf99b4a30622bd90aefbbb282998c5d538472a98
kumarPraveen9 <gpk50626@gmail.com>
2022-12-26 13:14:08 +0530

    adding greeting file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Employee;
+
+namespace Emloyee
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program \n");
+            EmployeeAttendance.checkatt();
+        }
+    }
+}
