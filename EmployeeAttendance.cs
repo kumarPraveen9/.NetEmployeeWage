f044abf7d8e439121f9960e5530c437ecffbcb0a
kumarPraveen9 <gpk50626@gmail.com>
2022-12-26 14:57:58 +0530

    modifed for UC3

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -9,16 +9,20 @@
             
            
             Random random=new Random();
-            int check=random.Next(2);
+            int check=random.Next(3);
             if (Employeee == check)
             {
-                Console.WriteLine("\n employee is present");
+                Console.WriteLine("\n employee is FullTime");
                 
 
             }
+            else if(check==2)
+            {
+                Console.WriteLine("\n employee is PartTime");
+            }
             else
             {
-                Console.WriteLine("\n employee is absent");
+                Console.WriteLine("employee is absent");
             }
             return check;
             
