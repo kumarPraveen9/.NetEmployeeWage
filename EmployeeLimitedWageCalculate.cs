40683aff29d1716a28261119dbff88f977277ffd
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-27 13:47:31 +0530

    modified

--- EmployeeLimitedWageCalculate.cs
+++ EmployeeLimitedWageCalculate.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,66 +6,42 @@ using System.Threading.Tasks;
 
 namespace Employee
 {
-    public class EmployeeLimitedWageCalculate
+    public class class1111
     {
-        public static void Cal_limitedwage()
+        public static void Cal_lmtWage()
         {
             Console.WriteLine("Program for Calculating Employee Wage !!");
-
-            int wagePerHour = 20;
-            int workingHours = 8;
-            int workinglimitedHours = 0;
-            int Totalwork = 0;
-            int Employeee = 1;
-            int atten = EmployeeAttendance.checkatt(Employeee);
-            int days = 1;
-            int flag = 0;
-            while ((days <= 20) && (workinglimitedHours <= 100))
-            {
-                if (atten == 1)
-                {
-
-                    Totalwork = wagePerHour * workingHours;
-                    workinglimitedHours = workinglimitedHours + 8;
-                    
-                   
-                    if ((workinglimitedHours <= 100) && (days <= 20))
-                    {
-                        flag = 1;
-                          
-                        
-                    }
-                }
-                else if (atten == 2)
-                {
-                    Totalwork = wagePerHour * (workingHours / 2);
-                    workinglimitedHours = workinglimitedHours + 4;
-                    
-                    if ((workinglimitedHours <= 100) && (days <= 20))
-                    {
-                        flag = 2;
-                        
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("working hours : " + workinglimitedHours);
-                    Console.WriteLine("the number of day are :" + (days - 1));
-                    break;
-                }
-                days = days + 1;
-            }
-                if ((flag==2))
-                {
-                    Console.WriteLine("working hours : " + workinglimitedHours);
-                    Console.WriteLine("the number of day are :" + (days-1));
-                }
-                if ((flag==1))
-                {
-                    Console.WriteLine("working hours : " + (workinglimitedHours-8));
-                    Console.WriteLine("the number of day are :" + (days-2));
-                }
             
+            int workingHours = 8;
+            int Employeee = 1;
+            int emphours = 0;
+            int days = 1;
+            int maxdays = 20;
+            int maxhours = 100;
+            
+            while ((emphours<=maxhours)&&(days<=maxdays))
+            {
+                   int atten = EmployeeAttendance.checkatt(Employeee);
+
+                    if (atten == 1)
+                    {
+                        workingHours = 8;
+                    }
+                    else if (atten == 2)
+                    {
+                        workingHours = 4;
+                    }
+                    else
+                    {
+                    workingHours = 0;
+                    }
+                emphours+= workingHours;
+                Console.WriteLine("day::{0} employee_hour::{1}", days, emphours);
+                days++;
+            }
+
         }
     }
 }
+
+
