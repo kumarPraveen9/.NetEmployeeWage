fb9f3aa81742bf610fad9fbcd34b9b1651943809
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-27 11:50:57 +0530

    modified

--- MonthWage.cs
+++ MonthWage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,30 +16,36 @@ namespace Employee
             int workingHours = 8;
             int Totalwork = 0;
             int Employeee = 1;
-            int atten = EmployeeAttendance.checkatt(Employeee);
-            int MonthDays = 30;
-            int wageMonth = 0;
+            int MonthDays = 20;
+            
+            int Monthwork = 0;
+            for (int i = 0; i < MonthDays; i++)
+            {
+                int atten = EmployeeAttendance.checkatt(Employeee);
 
-            if (atten == 1)
-            {
-                //Console.WriteLine("\n employee is present");
-                Totalwork = wagePerHour * workingHours;
-                wageMonth = Totalwork * MonthDays;
-                Console.WriteLine("total work in day: " + Totalwork);
-                Console.WriteLine("total work in month: " + wageMonth);
+                if (atten == 1)
+                {
+                    Totalwork = wagePerHour * workingHours;
+                    Console.WriteLine("total work in day: " + Totalwork);
+                    Console.WriteLine();
+                   
+                }
+                else if (atten == 2)
+                {
+                    Totalwork = wagePerHour * (workingHours / 2);
+                    Console.WriteLine("total Part time work in day: " + Totalwork);
+                    Console.WriteLine();
+                    
+                }
+                else
+                {
+                    Totalwork = 0;
+                    Console.WriteLine("total work : " + Totalwork);
+                    Console.WriteLine();
+                }
+                Monthwork += Totalwork;
             }
-            else if (atten == 2)
-            {
-                Totalwork = wagePerHour * (workingHours / 2);
-                wageMonth = Totalwork * MonthDays;
-                Console.WriteLine("total Part time work in day: " + Totalwork);
-                Console.WriteLine("total Part time work in month: " + wageMonth);
-            }
-            else
-            {
-                //Console.WriteLine("\n employee is absent");
-                Console.WriteLine("total work : " + Totalwork);
-            }
+            Console.WriteLine("TOTAL MONTH WORK: " + Monthwork);
         }
     }
 }
