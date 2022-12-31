923a5b5c644ee88921424a939007abe910a1e68c
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-31 18:36:20 +0530

    Update EmployeeAttendance.cs

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -10,20 +10,7 @@
            
             Random random=new Random();
             int check=random.Next(3);
-            if (Employeee == check)
-            {
-                Console.WriteLine("\n employee is FullTime");
-                
-
-            }
-            else if(check==2)
-            {
-                Console.WriteLine("\n employee is PartTime");
-            }
-            else
-            {
-                Console.WriteLine("employee is absent");
-            }
+           
             
             return check;
             
